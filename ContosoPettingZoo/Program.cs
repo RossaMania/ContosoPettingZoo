c1e206335ac973c4cf244426b333a708c092e119
RossaMania <raclettenberg@gmail.com>
2024-06-08 21:23:48 -0500

    chore: Create a method with optional parameter to assign the petting zoo animals to groups. The number of groups is optional param.

--- ContosoPettingZoo/Program.cs
+++ ContosoPettingZoo/Program.cs
@@ -18,23 +18,40 @@ string[] pettingZoo =
     "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
 };
 
+RandomizeAnimals();
+string[,] group = AssignGroup();
+Console.WriteLine("School A");
+// PrintGroup(group);
+
 void RandomizeAnimals()
 {
+    Random random = new Random();
 
-Random random = new Random();
+    for (int i = 0; i < pettingZoo.Length; i++)
+    {
+        int r = random.Next(i, pettingZoo.Length);
 
-for (int i = 0; i < pettingZoo.Length; i++)
+        string temp = pettingZoo[r];
+        pettingZoo[r] = pettingZoo[i];
+        pettingZoo[i] = temp;
+    }
+}
+
+string[,] AssignGroup(int groups = 6)
 {
-    int r = random.Next(i, pettingZoo.Length);
+    string[,] result = new string[groups, pettingZoo.Length/groups];
+    int start = 0;
 
-    string temp = pettingZoo[i];
-    pettingZoo[i] = pettingZoo[r];
-    pettingZoo[r] = temp;
+    for (int i = 0; i < groups; i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i,j] = pettingZoo[start++];
+        }
+    }
+
+    return result;
 }
-
-}
-
-// string[,] group = AssignGroup();
 
 Console.WriteLine("School A");
 
